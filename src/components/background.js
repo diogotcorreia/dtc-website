9a2005ad31587f3be395b172d36dbd9888e69e33
Diogo Correia <me@diogotc.com>
2021-01-31 19:27:46 +0000

    Make particles interactive
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- src/components/background.js
+++ src/components/background.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
 import styled from 'styled-components';
+import Particles from 'react-tsparticles';
 
 class ParticlesBackground extends Component {
   constructor(props) {
@@ -27,13 +27,55 @@ class ParticlesBackground extends Component {
     return (
       <Particles
         className={className}
-        params={{
+        options={{
           particles: {
+            links: {
+              enable: true,
+              distance: 100,
+              opacity: 0.5,
+            },
             number: {
-              value: this.state.mobile ? 20 : 50,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: this.state.mobile ? 30 : 55,
             },
             size: {
               value: 3,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            opacity: {
+              value: 0.5,
+            },
+          },
+          interactivity: {
+            detectsOn: 'window',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'bubble',
+                parallax: {
+                  enable: true,
+                  force: 100,
+                  smooth: 10,
+                },
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                opacity: 1,
+                size: 6,
+              },
             },
           },
           retina_detect: true,
