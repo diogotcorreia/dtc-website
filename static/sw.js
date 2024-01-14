91eb603f4280c97968facf1b60d219bd7f60763e
Diogo Correia <me@diogotc.com>
2024-01-14 23:33:49 +0100

    fix: service worker unregister not working

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,16 @@
 // Unregister service worker from previous Gatsby website,
 // otherwise returning visitors can't see the new site
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
 
-self.addEventListener('activate', () => {
-  self.registration.unregister();
-  self.clients.matchAll({ type: 'window' }).then(clients => {
-    for (const client of clients) {
-      client.navigate(client.url);
-    }
-  });
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
 });
